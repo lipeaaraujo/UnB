6b04ba8d3a7d89365ee83e3a437bbc2d96635d59
LipeAKugel <felipearaujodff@gmail.com>
2024-05-23 01:15:48 -0300

    (sbd1) :bulb: adicionando exercicio extra da aula 10

---
+++ SBD1-UnB/exercises/aula10extra1/aula10extra1Apaga.sql
@@ -0,0 +1,22 @@
+-- -------- < aula10extra1 > --------
+--
+--                    SCRIPT DE DELEÇÃO (DDL)
+--
+-- Data Criacao ...........: 16/05/2024
+-- Autor(es) ..............: Felipe Amorim de Araújo,
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: aula10extra1
+--
+-- PROJETO => 01 Base de Dados
+--         => 02 Tabelas
+-- 
+-- Ultimas Alteracoes
+--
+-- ---------------------------------------------------------
+
+-- BASE DE DADOS
+USE aula10extra1;
+
+-- TABELAS
+DROP TABLE CIDADE;
+DROP TABLE ESTADO;
